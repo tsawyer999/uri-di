39b131e187943c6ea3227bc80f19c4a7ed30de0b
Tom Sawyer <12345678@hollox.net>
2024-12-29 17:41:02 -0500

    refactor(Program): extract logic to configure StructureMap

--- UriDi.Console/ApplicationRegistry.cs
+++ UriDi.Console/ApplicationRegistry.cs
@@ -13,39 +13,41 @@ namespace UriDi.Console
             var container = new Container();
             foreach (var entry in configurations)
             {
-                container.Configure(configure =>
-                {
-                    configure
-                        .For<ICustomersHttpClient>()
-                        .Use<CustomersesHttpClient>()
-                        .Ctor<RegionConfiguration>("configuration")
-                        .Is(entry.Value)
-                        .Named(entry.Key);
-
-                    configure
-                        .For<IInvoicesHttpClient>()
-                        .Use<InvoicesHttpClient>()
-                        .Ctor<RegionConfiguration>("configuration")
-                        .Is(entry.Value)
-                        .Named(entry.Key);
-
-                    configure
-                        .For<ICustomersService>()
-                        .Use<CustomersService>()
-                        .Ctor<ICustomersHttpClient>()
-                        .Is(c => c.GetInstance<ICustomersHttpClient>(entry.Key))
-                        .Named(entry.Key);
-
-                    configure
-                        .For<IInvoicesService>()
-                        .Use<InvoicesService>()
-                        .Ctor<IInvoicesHttpClient>()
-                        .Is(c => c.GetInstance<IInvoicesHttpClient>(entry.Key))
-                        .Named(entry.Key);
-                });
+                container.Configure(x => AddRegistry(x, entry.Key, entry.Value));
             }
 
             return container;
+        }
+
+        private static void AddRegistry(ConfigurationExpression configure, string region, RegionConfiguration configuration)
+        {
+            configure
+                .For<ICustomersHttpClient>()
+                .Use<CustomersesHttpClient>()
+                .Ctor<RegionConfiguration>("configuration")
+                .Is(configuration)
+                .Named(region);
+
+            configure
+                .For<IInvoicesHttpClient>()
+                .Use<InvoicesHttpClient>()
+                .Ctor<RegionConfiguration>("configuration")
+                .Is(configuration)
+                .Named(region);
+
+            configure
+                .For<ICustomersService>()
+                .Use<CustomersService>()
+                .Ctor<ICustomersHttpClient>()
+                .Is(c => c.GetInstance<ICustomersHttpClient>(region))
+                .Named(region);
+
+            configure
+                .For<IInvoicesService>()
+                .Use<InvoicesService>()
+                .Ctor<IInvoicesHttpClient>()
+                .Is(c => c.GetInstance<IInvoicesHttpClient>(region))
+                .Named(region);
         }
     }
 }
